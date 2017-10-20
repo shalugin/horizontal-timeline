cfd561a0e52da3def28f47c94adc026959f884a1
Alexandr Shalugin <shalugin.alexandr@gmail.com>
2017-10-21 01:00:16 +0300

    Enable strict check.

--- horizontal-timeline/horizontal-timeline.component.ts
+++ horizontal-timeline/horizontal-timeline.component.ts
@@ -1,19 +1,6 @@
-import {
-  Component,
-  ElementRef,
-  ViewChild,
-  Input,
-  ViewChildren,
-  QueryList,
-  AfterViewInit,
-  animate,
-  transition,
-  style,
-  trigger,
-  keyframes,
-  state
-} from "@angular/core";
-import { TimelineElement } from "./timeline-element";
+import { AfterViewInit, Component, ElementRef, Input, QueryList, ViewChild, ViewChildren, } from '@angular/core';
+import { TimelineElement } from './timeline-element';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'horizontal-timeline',
@@ -64,23 +51,101 @@ import { TimelineElement } from "./timeline-element";
   ]
 })
 export class HorizontalTimelineComponent implements AfterViewInit {
-  private timelineTotWidth: number = 0;
   prevLinkInactive: boolean = true;
   nextLinkInactive: boolean = false;
   loaded: boolean = false;
   selectedIndex: number = 0;
-
   @Input() eventsMinDistance: number = 80;
   @Input() timelineElements: TimelineElement[];
   @Input() dateFormat: string = 'dd.MM.yyyy';
   @Input() disabled: boolean = false;
   @Input() showContent: boolean = false;
-
   @ViewChild('timelineWrapper') timelineWrapper: ElementRef;
   @ViewChild('eventsWrapper') eventsWrapper: ElementRef;
   @ViewChild('fillingLine') fillingLine: ElementRef;
   @ViewChild('eventsContent') eventsContent: ElementRef;
   @ViewChildren('timelineEvents') timelineEvents: QueryList<ElementRef>;
+  private timelineTotWidth: number = 0;
+
+  private static pxToNumber(val: string): number {
+    return Number(val.replace('px', ''));
+  }
+
+  private static getElementWidth(element: Element): number {
+    const computedStyle = window.getComputedStyle(element);
+    if (!computedStyle.width) {
+      return 0;
+    }
+    return HorizontalTimelineComponent.pxToNumber(computedStyle.width);
+  }
+
+  private static updateFilling(selectedEvent: any, filling: any, totWidth: number) {
+    // change .filling-line length according to the selected event
+    let eventStyle = window.getComputedStyle(selectedEvent);
+    let eventLeft = eventStyle.getPropertyValue('left');
+    let eventWidth = eventStyle.getPropertyValue('width');
+    let eventLeftNum = HorizontalTimelineComponent.pxToNumber(eventLeft) + HorizontalTimelineComponent.pxToNumber(eventWidth) / 2;
+    let scaleValue = eventLeftNum / totWidth;
+    HorizontalTimelineComponent.setTransformValue(filling.nativeElement, 'scaleX', scaleValue);
+  }
+
+  private static parentElement(element: any, tagName: string) {
+    if (!element || !element.parentNode) {
+      return null;
+    }
+
+    let parent = element.parentNode;
+    while (true) {
+      if (parent.tagName.toLowerCase() == tagName) {
+        return parent;
+      }
+      parent = parent.parentNode;
+      if (!parent) {
+        return null;
+      }
+    }
+  }
+
+  private static getTranslateValue(timeline: Element) {
+    let timelineStyle = window.getComputedStyle(timeline);
+    let timelineTranslate = timelineStyle.getPropertyValue('-webkit-transform') ||
+      timelineStyle.getPropertyValue('-moz-transform') ||
+      timelineStyle.getPropertyValue('-ms-transform') ||
+      timelineStyle.getPropertyValue('-o-transform') ||
+      timelineStyle.getPropertyValue('transform');
+
+    let translateValue = 0;
+    if (timelineTranslate.indexOf('(') >= 0) {
+      let timelineTranslateStr = timelineTranslate
+        .split('(')[1]
+        .split(')')[0]
+        .split(',')[4];
+      translateValue = Number(timelineTranslateStr);
+    }
+
+    return translateValue;
+  }
+
+  private static setTransformValue(element: any, property: any, value: any) {
+    element.style['-webkit-transform'] = property + '(' + value + ')';
+    element.style['-moz-transform'] = property + '(' + value + ')';
+    element.style['-ms-transform'] = property + '(' + value + ')';
+    element.style['-o-transform'] = property + '(' + value + ')';
+    element.style['transform'] = property + '(' + value + ')';
+  }
+
+  private static dayDiff(first: any, second: any) {
+    return Math.round(second - first);
+  }
+
+  private static minLapse(elements: TimelineElement[]) {
+    let result: number = 0;
+    for (let i = 1; i < elements.length; i++) {
+      let distance = HorizontalTimelineComponent.dayDiff(elements[i - 1].date, elements[i].date);
+      result = result ? Math.min(result, distance) : distance;
+    }
+    return result;
+  }
 
   ngAfterViewInit(): void {
     if (this.timelineElements && this.timelineElements.length) {
@@ -94,17 +159,17 @@ export class HorizontalTimelineComponent implements AfterViewInit {
     }
   }
 
-  onPrevClick(event) {
+  onPrevClick(event: Event) {
     event.preventDefault();
     this.updateSlide(this.timelineTotWidth, 'prev');
   }
 
-  onNextClick(event) {
+  onNextClick(event: Event) {
     event.preventDefault();
     this.updateSlide(this.timelineTotWidth, 'next');
   }
 
-  onEventClick(event, selectedItem: TimelineElement) {
+  onEventClick(event: Event, selectedItem: TimelineElement) {
     event.preventDefault();
     if (this.disabled) {
       return;
@@ -134,32 +199,32 @@ export class HorizontalTimelineComponent implements AfterViewInit {
     this.loaded = true;
   }
 
-  updateSlide(timelineTotWidth, string) {
+  updateSlide(timelineTotWidth: number, string: string) {
     // retrieve translateX value of eventsWrapper
     let translateValue = HorizontalTimelineComponent.getTranslateValue(this.eventsWrapper.nativeElement);
-    let wrapperWidth = Number(window.getComputedStyle(this.timelineWrapper.nativeElement).width.replace('px', ''));
+    let wrapperWidth = HorizontalTimelineComponent.getElementWidth(this.timelineWrapper.nativeElement);
     // translate the timeline to the left('next')/right('prev')
-    if (string == 'next') {
+    if (string === 'next') {
       this.translateTimeline(translateValue - wrapperWidth + this.eventsMinDistance, wrapperWidth - timelineTotWidth)
     } else {
       this.translateTimeline(translateValue + wrapperWidth - this.eventsMinDistance, null);
     }
   }
 
-  updateTimelinePosition(string, element) {
+  updateTimelinePosition(string: string, element: Element) {
     // translate timeline to the left/right according to the position of the selected event
-    let eventStyle = window.getComputedStyle(element, null);
-    let eventLeft = Number(eventStyle.getPropertyValue('left').replace('px', ''));
-    let timelineWidth = Number(window.getComputedStyle(this.timelineWrapper.nativeElement).width.replace('px', ''));
-    let timelineTotWidth = Number(window.getComputedStyle(this.eventsWrapper.nativeElement).width.replace('px', ''));
+    let eventStyle = window.getComputedStyle(element);
+    let eventLeft = HorizontalTimelineComponent.pxToNumber(eventStyle.getPropertyValue('left'));
+    let timelineWidth = HorizontalTimelineComponent.getElementWidth(this.timelineWrapper.nativeElement);
+    let timelineTotWidth = HorizontalTimelineComponent.getElementWidth(this.eventsWrapper.nativeElement);
     let timelineTranslate = HorizontalTimelineComponent.getTranslateValue(this.eventsWrapper.nativeElement);
 
-    if ((string == 'next' && eventLeft > timelineWidth - timelineTranslate) || (string == 'prev' && eventLeft < -timelineTranslate)) {
+    if ((string === 'next' && eventLeft > timelineWidth - timelineTranslate) || (string === 'prev' && eventLeft < -timelineTranslate)) {
       this.translateTimeline(-eventLeft + timelineWidth / 2, timelineWidth - timelineTotWidth);
     }
   }
 
-  translateTimeline(value: number, totWidth: number) {
+  translateTimeline(value: number, totWidth: number | null) {
     // only negative translate value
     value = (value > 0) ? 0 : value;
     // do not translate more than timeline width
@@ -170,14 +235,16 @@ export class HorizontalTimelineComponent implements AfterViewInit {
     this.nextLinkInactive = value == totWidth;
   }
 
-  private static updateFilling(selectedEvent, filling, totWidth) {
-    // change .filling-line length according to the selected event
-    let eventStyle = window.getComputedStyle(selectedEvent, null);
-    let eventLeft = eventStyle.getPropertyValue("left");
-    let eventWidth = eventStyle.getPropertyValue("width");
-    let eventLeftNum = Number(eventLeft.replace('px', '')) + Number(eventWidth.replace('px', '')) / 2;
-    let scaleValue = eventLeftNum / totWidth;
-    HorizontalTimelineComponent.setTransformValue(filling.nativeElement, 'scaleX', scaleValue);
+  setTimelineWidth(elements: TimelineElement[], width: number, eventsMinLapse: number) {
+    let timeSpan = HorizontalTimelineComponent.dayDiff(elements[0].date, elements[elements.length - 1].date);
+    let timeSpanNorm = timeSpan / eventsMinLapse;
+    timeSpanNorm = Math.round(timeSpanNorm) + 4;
+    let totalWidth = timeSpanNorm * width;
+    this.eventsWrapper.nativeElement.style.width = totalWidth + 'px';
+    let aHref = this.eventsWrapper.nativeElement.querySelectorAll('a.selected')[0];
+    HorizontalTimelineComponent.updateFilling(aHref, this.fillingLine, totalWidth);
+    this.updateTimelinePosition('next', aHref);
+    return totalWidth;
   }
 
   private setDatePosition(elements: TimelineElement[], min: number, eventsMinLapse: number) {
@@ -194,79 +261,5 @@ export class HorizontalTimelineComponent implements AfterViewInit {
       span.style.left = distanceNorm * min + (aWidth - spanWidth) / 2 + 'px';
       i++;
     }
-  }
-
-  setTimelineWidth(elements: TimelineElement[], width, eventsMinLapse: number) {
-    let timeSpan = HorizontalTimelineComponent.dayDiff(elements[0].date, elements[elements.length - 1].date);
-    let timeSpanNorm = timeSpan / eventsMinLapse;
-    timeSpanNorm = Math.round(timeSpanNorm) + 4;
-    let totalWidth = timeSpanNorm * width;
-    this.eventsWrapper.nativeElement.style.width = totalWidth + 'px';
-    let aHref = this.eventsWrapper.nativeElement.querySelectorAll('a.selected')[0];
-    HorizontalTimelineComponent.updateFilling(aHref, this.fillingLine, totalWidth);
-    this.updateTimelinePosition('next', aHref);
-    return totalWidth;
-  }
-
-  private static getElementWidth(element): number {
-    return Number(window.getComputedStyle(element, null).width.replace('px', ''));
-  }
-
-  private static parentElement(element, tagName: string) {
-    if (!element || !element.parentNode) {
-      return null;
-    }
-
-    let parent = element.parentNode;
-    while (true) {
-      if (parent.tagName.toLowerCase() == tagName) {
-        return parent;
-      }
-      parent = parent.parentNode;
-      if (!parent) {
-        return null;
-      }
-    }
-  }
-
-  private static getTranslateValue(timeline) {
-    let timelineStyle = window.getComputedStyle(timeline, null);
-    let timelineTranslate = timelineStyle.getPropertyValue("-webkit-transform") ||
-      timelineStyle.getPropertyValue("-moz-transform") ||
-      timelineStyle.getPropertyValue("-ms-transform") ||
-      timelineStyle.getPropertyValue("-o-transform") ||
-      timelineStyle.getPropertyValue("transform");
-
-    let translateValue = 0;
-    if (timelineTranslate.indexOf('(') >= 0) {
-      let timelineTranslateStr = timelineTranslate
-        .split('(')[1]
-        .split(')')[0]
-        .split(',')[4];
-      translateValue = Number(timelineTranslateStr);
-    }
-
-    return translateValue;
-  }
-
-  private static setTransformValue(element, property, value) {
-    element.style["-webkit-transform"] = property + "(" + value + ")";
-    element.style["-moz-transform"] = property + "(" + value + ")";
-    element.style["-ms-transform"] = property + "(" + value + ")";
-    element.style["-o-transform"] = property + "(" + value + ")";
-    element.style["transform"] = property + "(" + value + ")";
-  }
-
-  private static dayDiff(first, second) {
-    return Math.round(second - first);
-  }
-
-  private static minLapse(elements: TimelineElement[]) {
-    let result: number;
-    for (let i = 1; i < elements.length; i++) {
-      let distance = HorizontalTimelineComponent.dayDiff(elements[i - 1].date, elements[i].date);
-      result = result ? Math.min(result, distance) : distance;
-    }
-    return result;
   }
 }
